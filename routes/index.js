ec17c86486538e2799cc97f38c8f7d7567018796
Donald Lee <donaldlee50@gmail.com>
2015-09-17 15:51:47 -0700

    2 routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-
 var express = require('express');
 var router = express.Router();
 var icalendar = require('icalendar')
@@ -12,127 +11,210 @@ router.get('/', function(req, res, next) {
 	  } else {
 	  	var ical = new icalendar.parse_calendar(data);
 	  	var events = ical.events();
-	  	events.forEach (function (e){
-	  		// if (e.properties["UID"]) {
-	  		// 	console.log(e.properties["UID"][0]["value"]);
-	  		// }
-	  		// if (e.properties["ATTACH"]) {
-	  		// 	console.log(e.properties["ATTACH"][0]["value"]);
-	  		// }
-	  		// if (e.properties["ATTENDEE"]) {
-	  		// 	console.log(e.properties["ATTENDEE"][0]["value"]);
-	  		// }
-	  		// if (e.properties["DESCRIPTION"]) {
-	  		// 	console.log(e.properties["DESCRIPTION"][0]["value"]);
-	  		// }
-	  		// if (e.properties["DTSTAMP"]) {
-	  		// 	console.log(e.properties["DTSTAMP"][0]["value"]);
-	  		// }
-	  		// if (e.properties["DTSTART"]) {
-	  		// 	console.log(e.properties["DTSTART"][0]["value"]);
-	  		// }
-	  		// if (e.properties["DTEND"]) {
-	  		// 	console.log(e.properties["DTEND"][0]["value"]);
-	  		// }
-	  		// if (e.properties["CONTACT"]) {
-	  		// 	console.log(e.properties["CONTACT"][0]["value"]);
-	  		// }
-	  		// if (e.properties["COMMENT"]) {
-	  		// 	console.log(e.properties["COMMENT"][0]["value"]);
-	  		// }
-	  		// if (e.properties["CREATED"]) {
-	  		// 	console.log(e.properties["CREATED"][0]["value"]);
-	  		// }
-	  		// if (e.properties["CLASS"]) {
-	  		// 	console.log(e.properties["CLASS"][0]["value"]);
-	  		// }
-	  		// if (e.properties["DURATION"]) {
-	  		// 	console.log(e.properties["DURATION"][0]["value"]);
-	  		// }
-	  		// if (e.properties["EXDATE"]) {
-	  		// 	console.log(e.properties["EXDATE"][0]["value"]);
-	  		// }
-	  		// if (e.properties["RXRULE"]) {
-	  		// 	console.log(e.properties["RXRULE"][0]["value"]);
-	  		// }
-	  		// if (e.properties["RRULE"]) {
-	  		// 	console.log(e.properties["RRULE"][0]["value"]);
-	  		// }
-	  		// if (e.properties["RDATE"]) {
-	  		// 	console.log(e.properties["RDATE"][0]["value"]);
-	  		// }
-	  		// if (e.properties["RECURRENCE-ID"]) {
-	  		// 	console.log(e.properties["RECURRENCE-ID"][0]["value"]);
-	  		// }
-	  		// if (e.properties["RELATED-TO"]) {
-	  		// 	console.log(e.properties["RELATED-TO"][0]["value"]);
-	  		// }
-	  		// if (e.properties["RESOURCES"]) {
-	  		// 	console.log(e.properties["RESOURCES"][0]["value"]);
-	  		// }
-	  		// if (e.properties["REQUESTED-STATUS"]) {
-	  		// 	console.log(e.properties["REQUESTED-STATUS"][0]["value"]);
-	  		// }
-	  		// if (e.properties["STATUS"]) {
-	  		// 	console.log(e.properties["STATUS"][0]["value"]);
-	  		// }
-	  		// if (e.properties["TRANSP"]) {
-	  		// 	console.log(e.properties["TRANSP"][0]["value"]);
-	  		// }
-	  		// if (e.properties["PRIORITY"]) {
-	  		// 	console.log(e.properties["PRIORITY"][0]["value"]);
-	  		// }
-	  		// if (e.properties["ORGANIZER"]) {
-	  		// 	console.log(e.properties["ORGANIZER"][0]["value"]);
-	  		// }
-	  		// if (e.properties["LOCATION"]) {
-	  		// 	console.log(e.properties["LOCATION"][0]["value"]);
-	  		// }	  		
-	  		// if (e.properties["SEQUENCE"]) {
-	  		// 	console.log(e.properties["SEQUENCE"][0]["value"]);
-	  		// }
-	  		// if (e.properties["SUMMARY"]) {
-	  		// 	console.log(e.properties["SUMMARY"][0]["value"]);
-	  		// }
-	  		// if (e.properties["URL"]) {
-	  		// 	console.log(e.properties["URL"][0]["value"]);
-	  		// }
-	  		// if (e.properties["LAST-MODIFIED"]) {
-	  		// 	console.log(e.properties["LAST-MODIFIED"][0]["value"]);	
-	  		// }
-	  		// if (e.properties["X-TICKETS-URL"]) {
-	  		// 	console.log(e.properties["X-TICKETS-URL"][0]["value"]);
-	  		// }
-	  		// if (e.properties["X-INSTANT-EVENT"]) {
-	  		// 	console.log(e.properties["X-INSTANT-EVENT"][0]["value"]);
-	  		// }
-	  		// if (e.properties["X-COST"]) {
-	  		// 	console.log(e.properties["X-COST"][0]["value"]);
-	  		// }
 
-	  		if (e.properties["CATEGORIES"]) {
-	  			var categories = String(e.properties["CATEGORIES"][0]["value"]).split("\,");
-	  			for (var i in categories){
-	  				categories[i] = categories[i].replace("\\", "");
-	  				categories[i] = decodeURI(categories[i]);
-	  			};
-	  			console.log(categories);
+	  	var app = express
+	  	var orm = require('orm');
+
+		orm.connect('mysql://root:admin@127.0.0.1/timely-dev', function(err, db) {
+		  	if (err) return console.error('Connection error: ' + err);
+
+		  	var Event = db.define( 'event', {
+				id:  {type: 'serial', key: true}, // the auto-incrementing primary key
+				attach: {type: 'text'},
+				attendee: {type: 'text'},
+				uid: {type: 'text'},
+				comment: {type: 'text'},
+				contact: {type: 'text'},
+				description: {type: 'text', size: 5000 },
+				created: {type: 'date', time: true},
+				dtstamp: {type: 'date', time: true},
+				dtstart: {type: 'date', time: true},
+				dtend: {type: 'date'},
+				duration: {type: 'text'},
+				exdate: {type: 'date' },
+				rxrule: {type: 'text'},
+				last_modified: {type: 'text'},
+				sequence: {type: 'integer'},
+				summary: {type: 'text', size: 5000 },
+				rdate: {type: 'date'},
+				exception_date: {type: 'date'},
+				recurrence_id: {type: 'integer'},
+				related_to: {type: 'text'},
+				resources: {type: 'text'},
+				rrule: {type: 'text'},
+				exception_rule: {type: 'text'},
+				request_status: {type: 'text'},
+				status: {type: 'text'},
+				transp: {type: 'text'},
+				timezone_name: {type: 'text'},
+				allday: {type: 'text'},
+				priority: {type: 'text'},
+				organizer: {type: 'text'},
+				url: {type: 'text'},
+				x_tickets_url: {type: 'text'},
+				x_instant_event: {type: 'text'},
+				x_cost: {type: 'number'},
+				x_class: {type: 'text'},
+				show_map: {type: 'boolean'},
+				show_coordinates: {type: 'boolean'},
+				venue_id: {type: 'integer'},
+				organizer_id: {type: 'integer'},
+				ical_id: {type: 'integer'},
+				parent_event_id: {type: 'integer'},
+			});
+			
+			var Category = db.define( 'category', {
+				id:  {type: 'serial', key: true},
+				category_name: {type: 'text'}
+			});
+
+			Event.hasMany('categories', Category, { why: String }, { reverse: 'events', key: true })
+
+			var Venue = db.define( 'venue', {
+				id:  {type: 'serial', key: true},
+				name: {type: 'text'},
+				address: {type: 'text'},
+				city: {type: 'text'},
+				province: {type: 'text'},
+				postal_code: {type: 'text'},
+				country: {type: 'text'},
+				longitude: {type: 'text'},
+				latitude: {type: 'text'},
+			});
+
+			Event.hasOne("venue", Venue)
+		});
+
+		events.forEach (function (e){
+	  		var newRecord = {};
+
+	  		if (e.properties["UID"]) {
+	  			newRecord.uid = e.properties["UID"][0]["value"];
+	  		}
+	  		if (e.properties["ATTACH"]) {
+	  			newRecord.attach = e.properties["ATTACH"][0]["value"];
+	  		}
+	  		if (e.properties["ATTENDEE"]) {
+	  			newRecord.attendee = e.properties["ATTENDEE"][0]["value"];
+	  		}
+	  		if (e.properties["DESCRIPTION"]) {
+	  			newRecord.description = e.properties["DESCRIPTION"][0]["value"];
+	  		}
+	  		if (e.properties["DTSTAMP"]) {
+	  			newRecord.dtstamp = e.properties["DTSTAMP"][0]["value"];
+	  		}
+	  		if (e.properties["DTSTART"]) {
+	  			newRecord.dtstart = e.properties["DTSTART"][0]["value"];
+	  		}
+	  		if (e.properties["DTEND"]) {
+	  			newRecord.dtend = e.properties["DTEND"][0]["value"];
+	  		}
+	  		if (e.properties["CONTACT"]) {
+	  			newRecord.contact = e.properties["CONTACT"][0]["value"];
+	  		}
+	  		if (e.properties["COMMENT"]) {
+	  			newRecord.comment = e.properties["COMMENT"][0]["value"];
+	  		}
+	  		if (e.properties["CREATED"]) {
+	  			newRecord.created = e.properties["CREATED"][0]["value"];
+	  		}
+	  		if (e.properties["CLASS"]) {
+	  			newRecord.x_class= e.properties["CLASS"][0]["value"];
+	  		}
+	  		if (e.properties["DURATION"]) {
+	  			newRecord.duration = e.properties["DURATION"][0]["value"];
+	  		}
+	  		if (e.properties["EXDATE"]) {
+	  			newRecord.exdate = e.properties["EXDATE"][0]["value"];
+	  		}
+	  		if (e.properties["RXRULE"]) {
+	  			newRecord.rxrule = e.properties["RXRULE"][0]["value"];
+	  		}
+	  		if (e.properties["RRULE"]) {
+	  			newRecord.rrule = e.properties["RRULE"][0]["value"];
+	  		}
+	  		if (e.properties["RDATE"]) {
+	  			newRecord.rdate = e.properties["RDATE"][0]["value"];
+	  		}
+	  		if (e.properties["RECURRENCE-ID"]) {
+	  			newRecord.recurrrence_id = e.properties["RECURRENCE-ID"][0]["value"];
+	  		}
+	  		if (e.properties["RELATED-TO"]) {
+	  			newRecord.related_to = e.properties["RELATED-TO"][0]["value"];
+	  		}
+	  		if (e.properties["RESOURCES"]) {
+	  			newRecord.resources = e.properties["RESOURCES"][0]["value"];
+	  		}
+	  		if (e.properties["REQUESTED-STATUS"]) {
+	  			newRecord.requested_status = e.properties["REQUESTED-STATUS"][0]["value"];
+	  		}
+	  		if (e.properties["STATUS"]) {
+	  			newRecord.status = e.properties["STATUS"][0]["value"];
+	  		}
+	  		if (e.properties["TRANSP"]) {
+	  			newRecord.transp = e.properties["TRANSP"][0]["value"];
+	  		}
+	  		if (e.properties["PRIORITY"]) {
+	  			newRecord.priority = e.properties["PRIORITY"][0]["value"];
+	  		}
+	  		if (e.properties["ORGANIZER"]) {
+	  			newRecord.organizer = e.properties["ORGANIZER"][0]["value"];
+	  		}
+	  		if (e.properties["LOCATION"]) {
+	  			newRecord.location = e.properties["LOCATION"][0]["value"];
+	  		}	  		
+	  		if (e.properties["SEQUENCE"]) {
+	  			newRecord.sequence = e.properties["SEQUENCE"][0]["value"];
+	  		}
+	  		if (e.properties["SUMMARY"]) {
+	  			newRecord.summary = e.properties["SUMMARY"][0]["value"];
+	  		}
+	  		if (e.properties["URL"]) {
+	  			newRecord.url = e.properties["URL"][0]["value"];
+	  		}
+	  		if (e.properties["LAST-MODIFIED"]) {
+	  			newRecord.last_modified = e.properties["LAST-MODIFIED"][0]["value"];	
+	  		}
+	  		if (e.properties["X-TICKETS-URL"]) {
+	  			newRecord.x_tickets_url = e.properties["X-TICKETS-URL"][0]["value"];
+	  		}
+	  		if (e.properties["X-INSTANT-EVENT"]) {
+	  			newRecord.x_instant_event = e.properties["X-INSTANT-EVENT"][0]["value"];
+	  		}
+	  		if (e.properties["X-COST"]) {
+	  			newRecord.x_cost = e.properties["X-COST"][0]["value"];
 	  		}
 
-	  		if (e.properties["GEO"]) {
-	  			var geo = String(e.properties["GEO"][0]["value"]).split(";")
-	  			console.log(geo);
-	  		}
-	  		// console.log(console.log(e.properties);
+	  		db.sync(function(err) {
+				if (err) throw err;
+			
+				Event.create(newRecord, function(err, results){
+	  				if (err) throw err;
+	  			});
+			});
+	  		
+
+	  		// if (e.properties["CATEGORIES"]) {
+	  		// 	var categories = String(e.properties["CATEGORIES"][0]["value"].split("\,");
+	  		// 	for (var i in categories){
+	  		// 		categories[i] = categories[i].replace("\\", "");
+	  		// 		categories[i] = decodeURI(categories[i]);
+	  		// 	};
+	  		// 	console.log(categories);
+	  		// }
+
+	  		// if (e.properties["GEO"]) {
+	  		// 	var geo = String(e.properties["GEO"][0]["value"].split(";")
+	  		// 	console.log(geo);
+	  		// }
+	  		// console.log(= e.properties);
 	  	});
+
+		console.log('Done');
+		
 	  } 
 	});
-
-
-
   	res.render('index', { title: 'Expression' });
-
-
 });
 
 module.exports = router;
